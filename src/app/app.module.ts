0804f162896e39695cd2d4658cb881e1fc36b712
monjurulislam98 <134132906+monjurulislam98@users.noreply.github.com>
2023-05-20 14:41:04 +0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { Form2Component } from './form2/form2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    Form2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
